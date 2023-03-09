a2e863f2c5f07bea00848d3950902b0fbc81392c
Steven Mendoza <smendoza171@gmail.com>
2023-03-08 22:50:17 -0800

    Added bg images and image search functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,51 @@
-import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import Searchbar from "./components/Searchbar/Searchbar"
+import { useState, useEffect } from "react";
+import "./App.css";
+import Searchbar from "./components/Searchbar/Searchbar";
 
 function App() {
+  const [imageData, setImageData] = useState<any>(null);
+  const [bgImageUrl, setBgImageUrl] = useState(updateBackground);
+
+  function updateBackground() {
+    let url = `https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=${"night"}`;
+
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setBgImageUrl(data.urls.regular);
+        setImageData(data);
+      })
+      .catch((err) => {
+        console.error(err);
+        let url: any =
+          "https://images.unsplash.com/photo-1519608487953-e999c86e7455?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80";
+        setBgImageUrl(url);
+      });
+  }
 
   return (
     <div className="App">
-      <h1>Universal Search Engine</h1>
+      <div
+        className="bg"
+        style={{
+          backgroundImage: `url(${bgImageUrl})`,
+        }}
+      ></div>
+      {imageData && (
+        <div className="bg-author">
+          <a
+            target="_blank"
+            href={"https://unsplash.com/photos/%s".replace("%s", imageData.id)}
+          >
+            Photo by: {imageData.user.name}
+          </a>
+        </div>
+      )}
+
+      <h1 className="title">Universal Search Engine</h1>
       <Searchbar />
     </div>
   );
 }
 
-export default App
-
-
-
+export default App;
--- src/components/Searchbar/Searchbar.tsx
+++ src/components/Searchbar/Searchbar.tsx
@@ -1,91 +1,96 @@
+import { useEffect, useState } from "react";
 import "./index.css";
-import { useState, useEffect } from "react";
 
 const Searchbar = () => {
-  // Replace %s with query
   const searchEngines = [
     {
       name: "Google",
       homepage: "https://www.google.com/",
-      searchURL: "https://www.google.com/search?q=",
+      searchURL: "https://www.google.com/search?q=%s",
       imageSearchURL: "https://www.google.com/search?q=%s&tbm=isch",
     },
     {
       name: "Brave",
       homepage: "https://search.brave.com/",
-      searchURL: "https://search.brave.com/search?q=",
+      searchURL: "https://search.brave.com/search?q=%s",
       imageSearchURL: "https://search.brave.com/images?q=%s",
     },
     {
       name: "Bing",
       homepage: "https://www.bing.com/",
-      searchURL: "https://www.bing.com/search?q=",
+      searchURL: "https://www.bing.com/search?q=%s",
       imageSearchURL: "https://www.bing.com/images/search?q=%s",
     },
     {
       name: "Yahoo",
       homepage: "https://www.yahoo.com/",
-      searchURL: "https://www.search.yahoo.com/search?p=",
+      searchURL: "https://www.search.yahoo.com/search?p=%s",
       imageSearchURL: "https://images.search.yahoo.com/search/images?p=%s",
     },
     {
       name: "DuckDuckGo",
       homepage: "https://duckduckgo.com/",
-      searchURL: "https://duckduckgo.com/?q=",
+      searchURL: "https://duckduckgo.com/?q=%s",
       imageSearchURL: "https://duckduckgo.com/?q=%s&iax=images&ia=images",
     },
     {
       name: "Baidu",
       homepage: "https://www.baidu.com/",
-      searchURL: "https://www.baidu.com/s?wd=",
+      searchURL: "https://www.baidu.com/s?wd=%s",
       imageSearchURL:
         "https://image.baidu.com/search/index?tn=baiduimage&word=%s",
     },
     {
       name: "Yandex",
       homepage: "https://www.yandex.com/",
-      searchURL: "https://www.yandex.com/search/?text=",
+      searchURL: "https://www.yandex.com/search/?text=%s",
       imageSearchURL: "https://yandex.com/images/search?text=%s",
     },
     {
       name: "WolframAlpha",
       homepage: "https://www.wolframalpha.com/",
-      searchURL: "https://www.wolframalpha.com/input/?i=",
+      searchURL: "https://www.wolframalpha.com/input/?i=%s",
       imageSearchURL: "https://www.wolframalpha.com/input/?i=%s&t=image",
     },
     {
       name: "Ask",
       homepage: "https://www.ask.com/",
-      searchURL: "https://www.ask.com/web?q=",
+      searchURL: "https://www.ask.com/web?q=%s",
       imageSearchURL: "https://www.ask.com/images?q=%s",
     },
     {
       name: "Aol",
       homepage: "https://www.aol.com/",
-      searchURL: "https://search.aol.com/aol/search?q=",
+      searchURL: "https://search.aol.com/aol/search?q=%s",
       imageSearchURL: "https://www.search.aol.com/aol/image?q=%s",
     },
   ];
 
-  const defaultOption = searchEngines[0];
-  const storedOption = localStorage.getItem("selectedOption");
-  const initialOption = storedOption ? JSON.parse(storedOption) : defaultOption;
-  const [selectedOption, setSelectedOption] = useState(initialOption);
 
-  useEffect(() => {
-    const storedOption = localStorage.getItem("selectedOption");
-    if (storedOption) {
-      setSelectedOption(JSON.parse(storedOption));
-    } else {
-      setSelectedOption(searchEngines[0]);
-    }
-  }, []);
 
-  useEffect(() => {
-    localStorage.setItem("selectedOption", JSON.stringify(selectedOption));
-  }, [selectedOption]);
 
+
+  const [searchEngine, setSearchEngine] = useState(loadEngine);
+  const [searchMode, setSearchMode] = useState(loadMode);
   const [inputValue, setInputValue] = useState("");
+
+  function loadEngine() {
+    const defaultOption = searchEngines[0];
+    const storedOption = localStorage.getItem("engine");
+    const initialOption = storedOption
+      ? JSON.parse(storedOption)
+      : defaultOption;
+    return initialOption;
+  }
+  function loadMode() {
+    const defaultOption = "search";
+    const storedOption = localStorage.getItem("mode");
+    const initialOption = storedOption
+      ? JSON.parse(storedOption)
+      : defaultOption;
+    return initialOption;
+  }
+
 
   const handleChange = (e: any) => {
     if (e.target.name === "option") {
@@ -93,30 +98,75 @@ const Searchbar = () => {
       const selectedEngine: any = searchEngines.find(
         (engine) => engine.name === selectedName
       );
-      setSelectedOption(selectedEngine);
+      setSearchEngine(selectedEngine);
     } else {
       setInputValue(e.target.value);
     }
+  };
+
+  const changeEngine = (e: any) => {
+    const selectedName = e.target.value;
+    const selectedEngine: any = searchEngines.find(
+      (engine) => engine.name === selectedName
+    );
+    setSearchEngine(selectedEngine);
+  };
+
+  const changeSearchMode = (e: any) => {
+    const selectedMode = e.target.value;
+    setSearchMode(selectedMode);
   };
 
   const handleSubmit = (e: any) => {
     e.preventDefault();
     if (inputValue) {
       let engine: any = searchEngines.find(
-        (item: any) => item.name === selectedOption.name
+        (item: any) => item.name === searchEngine.name
       );
-      let searchUrl = `${engine.searchURL}${inputValue}`;
+      console.log(engine);
+
+      let searchUrl = ``;
+
+      if (searchMode === "search") {
+        searchUrl = engine.searchURL.replace("%s", inputValue);
+      } else if (searchMode === "image") {
+        searchUrl = engine.imageSearchURL.replace("%s", inputValue);
+      }
+
       window.location.href = searchUrl;
     }
   };
+
+  useEffect(() => {
+    const storedEngine = localStorage.getItem("engine");
+    if (storedEngine) {
+      setSearchEngine(JSON.parse(storedEngine));
+    } else {
+      setSearchEngine(searchEngines[0]);
+    }
+    const storedMode = localStorage.getItem("mode");
+    if (storedMode) {
+      setSearchMode(JSON.parse(storedMode));
+    } else {
+      setSearchMode("search");
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("engine", JSON.stringify(searchEngine));
+  }, [searchEngine]);
+  useEffect(() => {
+    localStorage.setItem("mode", JSON.stringify(searchMode));
+  }, [searchMode]);
 
   return (
     <div className="Searchbar">
       <form onSubmit={handleSubmit}>
         <select
           name="option"
-          value={selectedOption.name}
-          onChange={handleChange}
+          value={searchEngine.name}
+          onChange={changeEngine}
+          className="select-engine"
         >
           {searchEngines.map((engine) => (
             <option key={engine.name} value={engine.name}>
@@ -124,6 +174,17 @@ const Searchbar = () => {
             </option>
           ))}
         </select>
+
+        <select
+          name="type"
+          value={searchMode}
+          onChange={changeSearchMode}
+          className="select-mode"
+        >
+          <option value="search">Search</option>
+          <option value="image">Image</option>
+        </select>
+
         <input type="text" value={inputValue} onChange={handleChange} />
         <button onClick={handleSubmit}>
           <i className="fa-solid fa-magnifying-glass"></i>
